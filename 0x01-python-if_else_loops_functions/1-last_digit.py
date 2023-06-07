6a04217d2df04244c497594496e7459a442aa819
Elksass315 <qsasomar@gmail.com>
2023-06-07 13:39:18 -0700

    m

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last = int(str(number)[-1])
+last = int(str(abs(number))[-1])
 print(f"Last digit of {number} is {last} and is",end = " ")
 if last == 0:
     print("0")
