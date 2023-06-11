0bd7ac9176020634913946e84fa7036083fcbd74
Elksass315 <qsasomar@gmail.com>
2023-06-11 13:22:29 -0700

    mm

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,18 +1,10 @@
 #!/usr/bin/python3
-def add(a, b):
-    """My addition function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a + b
-    """
-    return (a + b)
-
-a = 1
-b= 2
 
 if __name__ == "__main__":
+    """print add"""
+    from add_0 import add
+
+    a = 1
+    b = 2
+
     print(f"{a} + {b} = {add(a, b)}")
