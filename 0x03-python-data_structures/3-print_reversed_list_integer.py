e705ccfe5cd0d282c136d2d34d3e768d6278657a
Elksass315 <qsasomar@gmail.com>
2023-06-12 16:54:46 -0700

    mm

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+#3-print_reversed_list_integer.py
+
+def print_reversed_list_integer(my_list=[]):
+    """print"""
+
+    i = len(my_list) - 1
+
+    while i >= 0:
+        print("{:d}".format(my_list[i]))
+        i = i - 1 
