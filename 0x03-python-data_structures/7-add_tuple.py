9cdd926f0c2e983507bfdcde2adc79f5219a0dd4
Elksass315 <qsasomar@gmail.com>
2023-06-12 17:37:01 -0700

    m

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,7 +2,7 @@
 # 7-add_tuple.py
 
 def add_tuple(tuple_a=(), tuple_b=()):
-     """Add two tuples."""
+    """Add"""
     if len(tuple_a) < 2:
         if len(tuple_a) == 0:
             tuple_a = 0, 0
