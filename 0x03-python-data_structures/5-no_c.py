0d8e82e48bb98c5665fce0fcc831c761dd90dab2
Elksass315 <qsasomar@gmail.com>
2023-06-12 17:14:18 -0700

    m

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# 5-no_c.py
+
+def no_c(my_string):
+    new = ""
+    c_list = ['c', 'C']
+
+    for i in my_string:
+        if i not in c_list:
+            new = new + i
+
+    return new
