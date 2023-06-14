48b533a15f58e6b0cf977607fa1792f4eaa8d80a
Elksass315 <qsasomar@gmail.com>
2023-06-14 15:00:37 -0700

    m

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+    lie = []
+    resulit = 0
+
+    for i in mu_list:
+        if i not in lie:
+            resulit = resulit + i
+            lie.append(i)
+    return resulit
