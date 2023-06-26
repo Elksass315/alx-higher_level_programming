b87b063e94553615941519ffd11006a43587b0e2
Elksass315 <qsasomar@gmail.com>
2023-06-27 01:37:40 +0300

    Elksass315

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    """prints the first x elements of a list and only integers.
+
+    args:
+        my_list (list): list of num to be print.
+        x (int): numper of elements to print.
+
+    Returns:
+        The number of elements printed.
+    """
+
+    r = 0
+
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            r+=1
+        except(ValueError, TypeError):
+            continue
+    print("")
+    return r
