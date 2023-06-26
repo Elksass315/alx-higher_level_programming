7ec5b3562fcb5ab6287b8c1b1b02c397366c8dba
Elksass315 <qsasomar@gmail.com>
2023-06-27 01:21:53 +0300

    Elksass315

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    """print integer
+
+    args:
+        value (int): the int to be print
+
+    Returns:
+        if any error occurs - False.
+        else - True.
+    """
+
+    try:
+        print("{:d}".format(value))
+        return(True)
+    except:
+        return(False)
