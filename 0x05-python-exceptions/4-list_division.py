3324ec21a1598dfe9823cc65614cf6aac2d8a6a9
Elksass315 <qsasomar@gmail.com>
2023-06-27 02:01:47 +0300

    Elksass315

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -17,5 +17,5 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
             r = 0
         finally:
-            new.append(div)
+            new.append(r)
     return (new)
