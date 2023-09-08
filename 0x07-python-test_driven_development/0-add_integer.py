41b8e86c32fa52ba6f0d879d01105ba5f55bef94
Elksass315 <qsasomar@gmail.com>
2023-09-08 22:11:08 +0300

    mm

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,7 +4,7 @@
 
 def add_integer(a, b=98):
     """function that adds 2 integers.
-    
+
     args: a (int) frist number.
           b (int) 2nd number.
     """
@@ -12,6 +12,6 @@ def add_integer(a, b=98):
     typeList = [int, float]
     if not type(a) in typeList:
         raise ValueError("a must be an integer")
-    if  not type(b) in typeList:
+    if not type(b) in typeList:
         raise ValueError("b must be an integer")
     return int(a) + int(b)
