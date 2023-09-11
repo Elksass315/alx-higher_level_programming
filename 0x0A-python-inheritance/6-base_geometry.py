52f14d332244cc50dc821af2541363018c7f38d2
Elksass315 <qsasomar@gmail.com>
2023-09-11 23:28:25 +0300

    Elksass

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines BaseGeometry."""
+
+
+class BaseGeometry:
+    """geometry base class."""
+
+    def area(self):
+        """No implemention."""
+        raise Exception("area() is not implemented")
