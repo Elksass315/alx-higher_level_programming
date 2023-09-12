f45e826039efdf2184e34e5cd22d04f6bdff107b
Elksass315 <qsasomar@gmail.com>
2023-09-12 21:40:20 +0300

    Elksass

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Student class."""
+
+
+class Student:
+    """student."""
+
+    def __init__(self, first_name, last_name, age):
+        """new Student.
+
+        Args:
+            first_name (str): student first name.
+            last_name (str): student last name.
+            age (int): student age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """dict representation of the Student.
+
+        If attrs is a list of strings, represents only those attributes
+        included in the list.
+
+        Args:
+            attrs (list): (Optional) The attributes to represent.
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replace all attributes of the Student class.
+
+        Args:
+            json (dict): The key/value pairs to replace .
+        """
+        for k, v in json.items():
+            setattr(self, k, v)
+
