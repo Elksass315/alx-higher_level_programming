853279c16848ecfecb5e8fed5f99caaec7c72ba2
Elksass315 <qsasomar@gmail.com>
2023-09-15 01:01:34 +0300

    Elksass

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """the “base” of all other classes in this project."""
+import json
 
 
 class Base:
@@ -24,3 +25,14 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """JSON string representation of list_dictionaries
+
+            args:
+                list_dictionaries (list): list of dictionaries to be JSON
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
