52f2e2416a667c57209f81c555fb4a935d9263dd
Elksass315 <qsasomar@gmail.com>
2023-09-15 01:09:16 +0300

    Elksass

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -36,3 +36,34 @@ class Base:
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """rites the JSON string representation of list_objs to a file.
+
+        Args:
+            list_objs (list): A list of inherited Base instances.
+        """
+        f = cls.__name__ + ".json"
+        with open(f, "w") as jf:
+            if list_objs is None:
+                jf.write("[]")
+            else:
+                list_dicts = [o.to_dictionary() for o in list_objs]
+                jf.write(Base.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Return JSON string.
+
+        Args:
+            json_string: is a string representing a list of dictionaries.
+        Returns:
+            If If json_string is None or empty, return an empty list
+            Otherwise, return the list represented by json_string
+        """
+        if json_string is None or json_string == "[]":
+            return []
+        return json.loads(json_string)
+
+
