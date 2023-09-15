170462677d803a90102d3ce40ef91092ccac1f1e
Elksass315 <qsasomar@gmail.com>
2023-09-16 00:34:16 +0300

    Elksass

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """the “base” of all other classes in this project."""
 import json
+import csv
+import turtle
 
 
 class Base:
@@ -96,3 +98,90 @@ class Base:
                 return [cls.create(**i) for i in listOfDict]
         except IOError:
             return []
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """serialization in CSV.
+            Format of the CSV:
+                Rectangle: <id>,<width>,<height>,<x>,<y>
+                Square: <id>,<size>,<x>,<y>
+
+        Args:
+            list_objs (list): list of obj.
+        """
+        name = cls.__name__ + ".csv"
+        with open(name, "w", newline="") as f:
+            if list_objs is None or list_objs == []:
+                f.write("[]")
+            else:
+                if cls.__name__ == "Rectangle":
+                    field = ["id", "width", "height", "x", "y"]
+                else:
+                    field = ["id", "size", "x", "y"]
+                writer = csv.DictWriter(f, fieldnames=field)
+                for obj in list_objs:
+                    writer.writerow(obj.to_dictionary())
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """deserializes in CSV.
+
+        Reads from `<cls.__name__>.csv`.
+
+        Returns:
+            If the file does not exist - an empty list.
+            Otherwise - a list of instantiated classes.
+        """
+        name = cls.__name__ + ".csv"
+        try:
+            with open(name, "r", newline="") as f:
+                if cls.__name__ == "Rectangle":
+                    field = ["id", "width", "height", "x", "y"]
+                else:
+                    field = ["id", "size", "x", "y"]
+                dic = csv.DictReader(f, fieldnames=field)
+                dic = [dict([k, int(v)] for k, v in d.items())for d in dic]
+                return [cls.create(**d) for d in dic]
+        except IOError:
+            return []
+
+    @staticmethod
+    def draw(list_rectangles, list_squares):
+        """Draw Rectangles and Squares using the turtle module.
+
+        Args:
+            list_rectangles (list): A list of Rectangle objects to draw.
+            list_squares (list): A list of Square objects to draw.
+        """
+        turt = turtle.Turtle()
+        turt.screen.bgcolor("#b7132c")
+        turt.pensize(7)
+        turt.shape("turtle")
+
+        turt.color("#aaaaff")
+        for rect in list_rectangles:
+            turt.showturtle()
+            turt.up()
+            turt.goto(rect.x, rect.y)
+            turt.down()
+            for i in range(2):
+                turt.forward(rect.width)
+                turt.left(90)
+                turt.forward(rect.height)
+                turt.left(90)
+            turt.hideturtle()
+
+        turt.color("#b5efd8")
+        for sq in list_squares:
+            turt.showturtle()
+            turt.up()
+            turt.goto(sq.x, sq.y)
+            turt.down()
+            for i in range(2):
+                turt.forward(sq.width)
+                turt.left(90)
+                turt.forward(sq.height)
+                turt.left(90)
+            turt.hideturtle()
+
+        turtle.exitonclick()
