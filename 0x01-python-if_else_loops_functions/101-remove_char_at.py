319f6f1cf3e554720d2e8687091c6c45a449f678
Elksass315 <qsasomar@gmail.com>
2023-10-11 21:41:22 +0300

    Elksass

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def remove_char_at(str, n):
+    output = ""
+    for i in range(len(str)):
+        if i == n:
+            output += " "
+        else:
+            output += str[i]
+    return (output)
