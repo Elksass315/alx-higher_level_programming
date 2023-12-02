88140dad7df0419ee44b02f13a362baa0b2c96dd
Elksass315 <qsasomar@gmail.com>
2023-12-02 23:38:11 +0200

    err handeling

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""send email using urllib"""
+
+
+if __name__ == "__main__":
+    import urllib.request
+    import urllib.error
+    import sys
+
+    url = sys.argv[1]
+    try:
+        with urllib.request.urlopen(url) as response:
+            html = response.read()
+            print(html.decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
