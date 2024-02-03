550bea05812a462eed6840a947a175680adf55af
Elksass <qsasomar@gmail.com>
2024-02-04 01:27:08 +0200

    initial

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" lists all states with a name starting with N (upper N)"""
+
+if __name__ == '__main__':
+    from sys import argv
+    import MySQLdb as mysql
+
+    try:
+        db = mysql.connect(host='localhost', port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3])
+    except Exception:
+        print('Failed to connect to the database')
+        exit(0)
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC;".format(argv[4]))
+
+    result_query = cursor.fetchall()
+
+    for row in result_query:
+        print(row)
+
+    cursor.close()
+    db.close()
