bd6ee971d5fcf55a7a9beac77b9cabd43ac5c203
Elksass <qsasomar@gmail.com>
2024-02-07 01:18:23 +0200

    update

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""prints the first State object from the database hbtn_0e_6_usa"""
+if __name__ == '__main__':
+    from model_state import Base, State
+    import sys
+    from sqlalchemy import (create_engine)
+    from sqlalchemy.orm import sessionmaker
+
+    co = f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}'
+    engine = create_engine(co, pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    session.query(State).where(State.id == 2)\
+            .update({'name': 'New Mexico'})
+    session.commit()
+    session.close()
+
