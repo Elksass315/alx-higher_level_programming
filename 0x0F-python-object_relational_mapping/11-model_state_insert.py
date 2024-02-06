f26ab4b5d01a2747b1125079ff1cec3e6c74a763
Elksass <qsasomar@gmail.com>
2024-02-07 01:10:51 +0200

    m

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""prints the first State object from the database hbtn_0e_6_usa"""
+if __name__ == '__main__':
+    from model_state import Base, State
+    import sys
+    from sqlalchemy import (create_engine)
+    from sqlalchemy.orm import sessionmaker
+
+    co = f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}'
+    engine = create_engine(co, pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    session.add(State(name = "Louisiana"))
+    session.flush()
