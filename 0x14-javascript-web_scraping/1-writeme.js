09e3a254d3caddb91d14f436c08dedd887295adf
Elksass <qsasomar@gmail.com>
2024-02-22 22:58:58 +0200

    semistandard Fix

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,8 +2,8 @@
 // reads and prints the content of a file.
 const fs = require('node:fs');
 
-fs.writeFile(process.argv[2], process.argv[3],err =>{
-  if(err){
-    console.log(err)
+fs.writeFile(process.argv[2], process.argv[3], err => {
+  if (err) {
+    console.log(err);
   }
-})
+});
